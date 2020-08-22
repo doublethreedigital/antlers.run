759b1c2a448f37e51263c8b3a01d31bdea25d143
Duncan McClean <duncan@mcclean.co.uk>
2020-08-22 17:57:54 +0100

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Antlers Fiddle</title>
-    <meta name="description" value="An online playground for Statamic's Antlers templating language.">
+    <meta name="description" value="An online playground for Antlers, Statamic's templating language.">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.57.0/codemirror.min.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
