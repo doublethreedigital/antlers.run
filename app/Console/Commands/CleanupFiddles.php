1383e0a8817adf63581adbe83d17fe5953926578
Duncan McClean <duncan@mcclean.co.uk>
2020-09-05 09:15:43 +0100

    Clean up fiddle views

---
+++ app/Console/Commands/CleanupFiddles.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use SplFileInfo;
+
+class CleanupFiddles extends Command
+{
+    protected $signature = 'antlers-fiddle:cleanup-fiddles';
+
+    protected $description = 'Clean up fiddles';
+
+    public function handle()
+    {
+        $this->info('Cleaing up fiddle views');
+
+        collect(File::allFiles(resource_path('views/fiddles')))
+            ->each(function (SplFileInfo $file) {
+                File::delete($file->getPathname());
+            });
+    }
+}
