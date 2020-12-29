40ec15a0e3f7f7775b49eadb2309c968d26f0edc
Duncan McClean <damcclean@users.noreply.github.com>
2020-12-29 09:38:44 +0000

    wip

--- resources/js/components/Modals/ShareModal.vue
+++ resources/js/components/Modals/ShareModal.vue
@@ -1,7 +1,7 @@
 <template>
     <base-modal @closed="$emit('closed', true)">
         <h2 class="font-semibold text-lg text-gray-800">Share Fiddle</h2>
-        <p v-if="sharing.errors" class="mb-2 text-gray-800 text-sm">There was an issue attempting to share your fiddle. Please try again later.</p>
+        <p v-if="sharing.errors && !sharing.success" class="mb-2 text-red-600 font-semibold text-sm">There was an issue attempting to share your fiddle. Please try again later.</p>
         <p v-if="sharing.success" class="mb-2 text-gray-800 text-sm">A sharing link for your fiddle has been generated and the link has been copied to your clipboard.</p>
 
         <pre v-if="sharing.success" class="my-2 bg-gray-200 p-2" v-text="sharing.url"></pre>
