5716d41a79b0c9f934b7257b5c2d01146a68ce45
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2021-03-27 13:27:58 +0000

    update domain

--- resources/js/components/Modals/InfoModal.vue
+++ resources/js/components/Modals/InfoModal.vue
@@ -1,7 +1,7 @@
 <template>
     <base-modal @closed="$emit('closed', true)">
         <h2 class="font-semibold text-lg text-gray-800">About Antlers.run</h2>
-        <p class="mb-2 text-gray-800 text-sm">Antlers.run is an online playground for Antlers, Statamic's templating language. Antlers.run is run by <a href="https://duncanm.dev">Duncan McClean</a> and has no affilation with Statamic or Wilderborn LLC.</p>
+        <p class="mb-2 text-gray-800 text-sm">Antlers.run is an online playground for Antlers, Statamic's templating language. Antlers.run is run by <a href="https://duncanmcclean.com">Duncan McClean</a> and has no affilation with Statamic or Wilderborn LLC.</p>
         <a class="text-gray-600 text-sm font-semibold" href="https://github.com/doublethreedigital/antlers.run">View on GitHub</a>
     </base-modal>
 </template>
