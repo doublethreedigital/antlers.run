9905a3f9e6d93b8ec6bf80339af3f6d07d8a734a
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2021-04-29 17:46:21 +0100

    wip

--- resources/js/components/Modals/InfoModal.vue
+++ resources/js/components/Modals/InfoModal.vue
@@ -3,7 +3,10 @@
         <h2 class="font-semibold text-lg text-gray-800">About Antlers.run</h2>
         <p class="mb-2 text-gray-800 text-sm">Antlers.run is an online playground for Antlers, Statamic's templating language. Antlers.run is run by <a href="https://duncanmcclean.com">Duncan McClean</a> and has no affilation with Statamic or Wilderborn LLC.</p>
         <a class="text-gray-600 text-sm font-semibold" href="https://github.com/doublethreedigital/antlers.run">View on GitHub</a>
-        <a href="https://www.digitalocean.com/?refcode=2b7296f7ed75&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"><img src="https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%202.svg" alt="DigitalOcean Referral Badge" /></a>
+            
+        <div class="mt-4 flex items-center justify-center">
+            <a href="https://www.digitalocean.com/?refcode=2b7296f7ed75&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"><img src="https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%202.svg" alt="DigitalOcean Referral Badge" /></a>    
+        </div>
     </base-modal>
 </template>
 
